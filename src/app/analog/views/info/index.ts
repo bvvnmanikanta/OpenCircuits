a78bf39b7165cd569bb7c136a6368175144ba4e5
BensonCheng8787 <bensoncheng888@gmail.com>
2022-10-28 15:58:31 -0400

    changed voltagesource to fit new format

--- src/app/analog/views/info/index.ts
+++ src/app/analog/views/info/index.ts
@@ -15,10 +15,13 @@ export const AnalogPropInfo: PropInfoRecord<AnalogObj> = {
         { id: "resistance", type: "float", key: "resistance", label: "Resistance", step: 100, min: 0 },
     ],
     "Ground": DefaultComponentPropInfo,
-    "VoltageSource":{
+    "VoltageSource":[
         ...DefaultComponentPropInfo,
-        "v" : {type: "float", label: "Voltage", step: 0.1, min: 0},
-        "v1": {type: "float", label: "Voltage High", step: 0.1, min: 0},
-        "waveform": {type: "string[]", label: "Waveform", options: [["DC", "a"],["Pulse", "b"],["Sine", "c"]]},//cant use string[] beacuse of readonly
-    }
+        { id: "v", type:"float", key: "v", label: "Voltage", step: 0.1, min: 0},
+        { id: "v1", type: "float", key: "v1", label: "Voltage High", step: 0.1, min: 0},
+        { id: "waveform", type: "string[]", key: "waveform", label: "Waveform", options: [["DC", "a"],["Pulse", "b"],["Sine", "c"]]},
+        //"v" : {type: "float", label: "Voltage", step: 0.1, min: 0},
+        //"v1": {type: "float", label: "Voltage High", step: 0.1, min: 0},
+        //"waveform": {type: "string[]", label: "Waveform", options: [["DC", "a"],["Pulse", "b"],["Sine", "c"]]},
+    ]
 };

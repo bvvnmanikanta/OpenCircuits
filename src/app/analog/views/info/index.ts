429cc7f96f6892c8bd2ebb8fd6da54282b32631b
BensonCheng8787 <bensoncheng888@gmail.com>
2022-10-30 15:21:29 -0400

    Update index.ts

--- src/app/analog/views/info/index.ts
+++ src/app/analog/views/info/index.ts
@@ -25,10 +25,32 @@ export const AnalogPropInfo: PropInfoRecord<AnalogObj> = {
         { id: "td", type: "float", key: "td", label: "Time Delay", step: 0.1, min:0},
         { id: "tr", type: "float", key: "tr", label: "Rise Time",  step: 0.1, min:0},
         { id: "tf", type: "float", key: "tf", label: "Fall Time",  step: 0.1, min:0},
-
-
+        { id: "pw", type: "float", key: "pw", label: "Pulse Width", step: 0.1, min:0},
         //"v" : {type: "float", label: "Voltage", step: 0.1, min: 0},
         //"v1": {type: "float", label: "Voltage High", step: 0.1, min: 0},
         //"waveform": {type: "string[]", label: "Waveform", options: [["DC", "a"],["Pulse", "b"],["Sine", "c"]]},
-    ]
+    ],
 };
+const ConstVPropInfo = {
+    id: "waveform-const-group",
+    type: "group",
+    isActive:  (states) => (states.every((state) => state["waveform"] == "DC")),
+    info: [{ id: "v", type:"float", key: "v", label: "Voltage", step: 0.1, min: 0}]
+}
+    
+
+const PulseVPropInfo = {
+    id: "waveform-pulse-group",
+    type: "group",
+    isActive: (states) => (states.every((state) => state["waveform"] === "DC")),
+    info: [
+    { id: "v", type:"float", key: "v", label: "Voltage", step: 0.1, min: 0},
+    { id: "v1", type: "float", key: "v1", label: "Voltage High", step: 0.1, min: 0},
+    { id: "td", type: "float", key: "td", label: "Time Delay", step: 0.1, min:0},
+    { id: "tr", type: "float", key: "tr", label: "Rise Time",  step: 0.1, min:0},
+    { id: "tf", type: "float", key: "tf", label: "Fall Time",  step: 0.1, min:0},
+    { id: "pw", type: "float", key: "pw", label: "Pulse Width", step: 0.1, min:0},
+    { id: "p", type: "float", key: "p", label: "Period", step: 0.1, min:0},
+    { id: "ph", type: "float", key: "ph", label: "Phase", step: 0.1, min:0}]
+}
+

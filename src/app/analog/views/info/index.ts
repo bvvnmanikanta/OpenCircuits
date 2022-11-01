220ee55367aac48e2795b9faeb9699f62fc12ff5
BensonCheng8787 <bensoncheng888@gmail.com>
2022-11-01 17:10:09 -0400

    Revert "added subgroups of voltagesource, not incorporated"
    This reverts commit f151beaa2a722cc25cb0f7fff33a4bd134757cc8.

--- src/app/analog/views/info/index.ts
+++ src/app/analog/views/info/index.ts
@@ -19,13 +19,35 @@ export const AnalogPropInfo: PropInfoRecord<AnalogObj> = {
 
     "VoltageSource":[
         ...DefaultComponentPropInfo,
-        { id: "v", type:"float", key: "v", label: "Voltage", step: 0.1, min: 0},
-        { id: "v1", type: "float", key: "v1", label: "Voltage High", step: 0.1, min: 0},
-        { id: "waveform", type: "string[]", key: "waveform", label: "Waveform", options: [["DC", "a"],["Pulse", "b"],["Sine", "c"]]},
-        { id: "td", type: "float", key: "td", label: "Time Delay", step: 0.1, min:0},
-        { id: "tr", type: "float", key: "tr", label: "Rise Time",  step: 0.1, min:0},
-        { id: "tf", type: "float", key: "tf", label: "Fall Time",  step: 0.1, min:0},
+        
+    ],
+};
+/*
+the three sub components for voltage source, they should be incorporated
+into voltagesource so that voltagesource would be a group object consists 
+of ConstVPropInfo, PulseVPropInfo, and SineVPropInfo.
+*/
 
+const ConstVPropInfo = {
+    id: "waveform-const-group",
+    type: "group",
+    //isActive:  (states) => (states.every((state) => state["waveform"] == "DC")),
+    info: [{ id: "v", type:"float", key: "v", label: "Voltage", step: 0.1, min: 0}]
+}
+
+
+const PulseVPropInfo = {
+    id: "waveform-pulse-group",
+    type: "group",
+    //isActive: (states) => (states.every((state) => state["waveform"] === "DC Pulse")),
+    info: [
+    { id: "v", type:"float", key: "v", label: "Voltage", step: 0.1, min: 0},
+    { id: "v1", type: "float", key: "v1", label: "Voltage High", step: 0.1, min: 0},
+    { id: "tf", type: "float", key: "tf", label: "Fall Time",  step: 0.1, min:0},
+    { id: "pw", type: "float", key: "pw", label: "Pulse Width", step: 0.1, min:0},
+    { id: "p", type: "float", key: "p", label: "Period", step: 0.1, min:0},
+    { id: "ph", type: "float", key: "ph", label: "Phase", step: 0.1, min:0},]
+}
 
 const SineVPropInfo ={
     id: "waveform-sine-group",
